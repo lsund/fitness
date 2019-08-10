10210983935e4e82cd0069437bb5c7f70f0243ad
Ludvig Sundström <lud.sund@gmail.com>
2019-08-10 21:17:44 +0200

    Document initial design goals

--- src/clj/fitness/handler.clj
+++ src/clj/fitness/handler.clj
@@ -7,6 +7,27 @@
             [ring.middleware.params :refer [wrap-params]]
             [fitness.render :as render]))
 
+;; Datbase changes:
+;;
+;; One type of exercise with nullable fields
+;; Exercise (Id, Name, Duration, Distance, Lowpulse, Highpulse,
+;;           level, sets, reps, weight, step)
+;;
+;; The exercise record denotes a done exercise. No need for multiple tables
+;; all data can be derived from this table.
+;;
+;; Still need table for SquashMatch and SquashOpponent
+;;
+;; Instead of trying to monitor exact growth, Show the weight max and
+;; average reps + weight and simply let the user enter the data
+;;
+;; Instead of plans, have a scroll list of exercises, the user adds it
+;; done exercises to the session and then clicks complete
+;;
+;; Plans are just lists, like suggestions: you don't need to follow them
+;; and no complicated skip/increment/decrement etc.
+;;
+
 (defn- app-routes [{:keys [db] :as config}]
   (routes
    (GET "/" []

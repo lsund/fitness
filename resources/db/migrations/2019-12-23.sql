7ae04cd8417a21f87b1ca3b895a69e03bdb6ec80
Ludvig Sundström <ludvig.sundstroem@innoq.com>
2019-12-25 11:38:42 +0100

    Add unique constraint in create table

--- resources/db/migrations/2019-12-23.sql
+++ resources/db/migrations/2019-12-23.sql
@@ -1,3 +1,2 @@
 create table exerciseid_name (id serial primary key, exerciseid int not null, name text not null);
 alter table exercise drop column name;
-ALTER TABLE exerciseid_name add constraint constraintname unique (exerciseid);
